2e4c2a7fb3172abf4612ce193f2a47d27d3cdb61
Nicholas Brigando <baabdieskaap@gmail.com>
2023-02-02 16:42:11 +0200

    Basic score analyzer - no positions

--- main.py
+++ main.py
@@ -1,6 +1,40 @@
 import numpy as np
 import FEN as FEN
 
+def scoreAnalyzer(boardIn):
+    score = 0
+
+    for x in range(8):
+        for y in range(8):
+            checkSquare = boardIn[y, x]
+            match checkSquare:
+                case "r":
+                    score -= 5
+                case "n":
+                    score -= 4
+                case "b":
+                    score -= 4
+                case "q":
+                    score -= 10
+                case "k":
+                    score += 0
+                case "p":
+                    score -= 1
+                case "R":
+                    score += 5
+                case "N":
+                    score += 4
+                case "B":
+                    score += 4
+                case "Q":
+                    score += 10
+                case "K":
+                    score += 0
+                case "P":
+                    score += 1
+
+    return score
+
 name = "Cultist"
 version = "0.0.1"
 print("Welcome to the basic " + name + " engine platform, v" + version + ".\n\n")
@@ -20,4 +54,7 @@ fullMove = int(options[4])
 print(board)
 print("Side to move: ", turnBW.upper())
 print("Move number: ", fullMove)
+
+currScore = scoreAnalyzer(board)
+print("Current analysis: ", currScore)
 
